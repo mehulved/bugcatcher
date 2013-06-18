f815af1383b435977f5d2e35cc81b0d51e96be6a
Mehul Ved <mehul.n.ved@gmail.com>
2013-06-19 00:41:03 +0530

    capture the POST data as variables

--- bug.py
+++ bug.py
@@ -1,9 +1,29 @@
 from flask import Flask
+from flask import request
+import simplejson as json
+
 app = Flask(__name__)
 
-@app.route('/')
+# Capture the bug data sent by the tool
+@app.route('/capture', methods=["POST"])
 def capture():
-    return "Capturing...";
+    error = None
+    if request.method == "POST":
+        data = json.loads(request.data)
+        title = data['entry']['label']
+        action = data['entry']['whatdone']
+        happened = data['entry']['whathad']
+        expected = data['entry']['whatshould']
+        security = data['entry']['security']
+        email = data['entry']['email']
+        browser = data['entry']['browser']
+        url = data['entry']['url']
+        screensize = data['entry']['screen']
+        scrollX = data['entry']['scrollX']
+        scrollY = data['entry']['scrollY']
+        console_log = data['entry']['console']
+        image = data['entry']['screenshot']
+    return render_template('success.html', error=error)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
